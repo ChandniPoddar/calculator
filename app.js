321876593023e91ea81c4c38a06a17ff5bded162
Chandni Poddar <poddarchandni5@gmail.com>
2024-12-08 11:24:17 +0530

    fist web project

---
+++ app.js
@@ -0,0 +1,25 @@
+
+let num = document.querySelectorl(".num");
+
+console.log("hello world")
+let a=0
+num.onclick=()=>{
+    if(num===1){
+        a=1
+        console.log("hello ")
+    };
+    if(num===2){
+        a=2
+    };if(num===3){
+        a=3
+    };if(num===4){
+        a=4
+    };if(num===1){
+        a=1
+    };if(num===1){
+        a=1
+    };if(num===1){
+        a=1
+    };
+
+}
